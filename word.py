cfb0a1ffec6b48d12d4a0b9a5ef3974df79cc82d
pdkusalkar <pdkusalkar@gmail.com>
2022-01-31 17:52:09 +0530

    no to word

---
+++ word.py
@@ -0,0 +1,23 @@
+n=int(input("enter no 0 to 9:"))
+if n==0:
+    print("Zero")
+elif n==1:
+    print("one")
+elif n==2:
+    print("two")
+elif n==3:
+    print("three")
+elif n==4:
+    print("four")
+elif n==5:
+    print("five")
+elif n==6:
+    print("six")
+elif n==7:
+    print("seven")
+elif n==8:
+    print("eight")
+elif n==9:
+    print("nine")
+else:
+    print("not in 0 to 9")
