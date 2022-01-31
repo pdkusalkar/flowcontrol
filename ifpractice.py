31d720138edc42cf8f5ee58a443c092bb743bc00
pdkusalkar <pdkusalkar@gmail.com>
2022-01-31 17:37:30 +0530

    if-elif-else

---
+++ ifpractice.py
@@ -0,0 +1,26 @@
+'''a=int(input("Enter 1st no:"))
+b=int(input("Enter 2nd no:"))
+
+if a<b:
+    print("a is less than b")
+else:
+    print("a is greter than b")'''
+
+
+a=int(input("enter 1st no:"))
+b=int(input("enter 2nd no:"))
+c=int(input("enter 3rd no:"))
+
+if a>b and a>c:
+    print("Greater no a")
+elif b>c:
+    print("grater no b")
+elif a==b:
+    print("a and b no are same")
+elif a==c:
+    print("a and c no are same")
+elif c==b:
+    print("c and b no are same")
+else:
+   print("g no c")
+
