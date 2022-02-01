881fdcf8058975dfdfc988f494ada54dce47564f
pdkusalkar <pdkusalkar@gmail.com>
2022-02-01 17:30:56 +0530

    all are for/while file

---
+++ transfer/break.py
@@ -0,0 +1,13 @@
+for i in range ( 10 ) :
+    if i == 5 :
+        print ( "processing is enough... plz break" )
+        break
+    print ( i )
+
+cart = [10, 20, 300, 400, 50]
+for item in cart :
+    if item > 350 :
+        print ( "order" )
+        break
+    print ( item )
+
---
+++ transfer/contiue.py
@@ -0,0 +1,20 @@
+for i in range ( 10 ) :
+    if i % 2==0 :
+        continue
+    print ( i )
+
+for i in range(100):
+    if i%9==0:
+        print(i)
+    else:pass
+
+
+for i in range(90):
+    if i%8==0:
+        print(i)
+    else:pass
+
+for i in range(60):
+    if i%4==0:
+        print(i)
+    else:pass
